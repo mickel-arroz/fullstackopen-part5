e615efb921199e47cd1eff2c60bbbb7b2ad97840
Mickel Arroz <arrozmickel@gmail.com>
2025-10-02 10:15:27 -0400

    5A Guardando el token en el local storage del navegador

--- notesApp/notes/src/App.jsx
+++ notesApp/notes/src/App.jsx
@@ -20,6 +20,15 @@ const App = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      noteService.setToken(user.token);
+    }
+  }, []);
+
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
@@ -61,8 +70,9 @@ const App = () => {
         username,
         password,
       });
-      noteService.setToken(user.token);
 
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user));
+      noteService.setToken(user.token);
       setUser(user);
       setUsername('');
       setPassword('');
