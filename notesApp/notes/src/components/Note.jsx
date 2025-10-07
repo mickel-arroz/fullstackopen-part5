81027dd85719c212cfbd8af88530b42ff2e43da2
Mickel Arroz <arrozmickel@gmail.com>
2025-10-07 11:54:08 -0400

    5C Clicando botones en las pruebas

--- notesApp/notes/src/components/Note.jsx
+++ notesApp/notes/src/components/Note.jsx
@@ -1,10 +1,10 @@
-const Note = ({ note, toggleImportant }) => {
-  const label = note.important ? "make not important" : "make important";
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important ? 'make not important' : 'make important';
 
   return (
     <li className="note">
       {note.content}
-      <button onClick={toggleImportant}>{label}</button>
+      <button onClick={toggleImportance}>{label}</button>
     </li>
   );
 };
