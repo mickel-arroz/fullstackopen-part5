bc18c2c7748cca67eeed00acfe364331838dc2bf
Mickel Arroz <arrozmickel@gmail.com>
2025-10-11 21:40:37 -0400

    5D Probando nuestro propio código

---
+++ testing-playwright/tests/note_app.spec.js
@@ -0,0 +1,16 @@
+const { test, expect } = require('@playwright/test');
+const { describe } = require('node:test');
+
+describe('Note app', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173');
+
+    const locator = await page.getByText('Notes');
+    await expect(locator).toBeVisible();
+    await expect(
+      page.getByText(
+        'Note app, Department of Computer Science, University of Helsinki 2025'
+      )
+    ).toBeVisible();
+  });
+});
