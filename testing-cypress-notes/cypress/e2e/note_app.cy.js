d51fe49656fbc31ee795f0311957a87f73775410
Mickel Arroz <arrozmickel@gmail.com>
2025-12-17 18:36:32 -0400

    5E Probando el formulario para agregar notas

--- testing-cypress-notes/cypress/e2e/note_app.cy.js
+++ testing-cypress-notes/cypress/e2e/note_app.cy.js
@@ -22,4 +22,18 @@ describe('Note app', function () {
 
     cy.contains('mluukkai logged-in');
   });
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.contains('log in').click();
+      cy.get('input:first').type('mluukkai');
+      cy.get('input:last').type('salainen');
+      cy.get('#login-button').click();
+    });
+    it('a new note can be created', function () {
+      cy.contains('new note').click();
+      cy.get('input').type('a note created by cypress');
+      cy.contains('save').click();
+      cy.contains('a note created by cypress');
+    });
+  });
 });
